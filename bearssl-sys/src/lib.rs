96e7e0936e52b4fa8f2115264a1b96e935349ca1
Aydin Mercan <aydin@mercan.dev>
2022-09-06 22:30:14 +0300

    clippy: explicitly allow unactionable lints
    * `TlsProfile` is better off sticking to the style used in standards.
    
    * `bearssl-sys` bindings can't be annotated with safety as they are
      autogenerated and have the same safety properties with the library
      itself.

--- bearssl-sys/src/lib.rs
+++ bearssl-sys/src/lib.rs
@@ -1,7 +1,8 @@
 #![no_std]
-#![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(clippy::missing_safety_doc)]
 
 #[cfg(feature = "dont-assume-size_t-equals-uintptr_t")]
 use libc::size_t;
